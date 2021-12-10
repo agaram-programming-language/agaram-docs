515ca58836a607ef8c5d073b955dbdbf6ff3e870
naveen <kmnaveen101@gmail.com>
2021-12-10 06:57:40 +0530

    add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'அகரம்'
 copyright = '2021, Graziella'
-author = 'Graziella'
+author = 'Naveen Muthusamy'
 
 release = '0.1'
 version = '0.1.0'
